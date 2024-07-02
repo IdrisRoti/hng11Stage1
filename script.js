241482c5c534ad9c1bbf28fea92ada160877d2a7
idrisRoti <idrisomisakin@gmail.com>
2024-07-02 13:06:36 +0100

    modify data attributes

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const currDay = document.querySelector(".day");
 const currTime = document.querySelector(".time");
-// const currentDay = document.querySelector(".day");
-// const currentUTCtime = document.querySelector(".time");
 
 const dayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 const today = new Date();
 const day = today.getDay();
+
 const hours = today.getUTCHours();
 const minutes = today.getUTCMinutes();
 const seconds = today.getUTCSeconds();
@@ -16,7 +15,4 @@ const dayString = dayArray[day];
 const time = hours + ":" + minutes + ":" + seconds + ":" + milliseconds;
 
 currDay.innerText = dayString;
-currDay.setAttribute("data-test-id", dayString)
-
 currTime.innerText = time;
-currTime.setAttribute("data-test-id", time)
